cee8e8265875c1b2822aa0b895af681b29b48c34
aryaaapplg <ibu.masarya@gmail.com>
2026-04-20 17:01:37 +0700

    #2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,25 +3,24 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+// Hapus baris use Illuminate\Database\Console\Seeds\WithoutModelEvents; kalau tidak dipakai
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Admin Absensi',
-            'email' => 'admin@absensi.com',
-            'password' => Hash::make('password')
-        ]);
+        // Gunakan updateOrCreate untuk memasukkan akun admin tanpa memanggil Factory
+        User::updateOrCreate(
+            ['email' => 'admin@absensi.com'], // Laravel akan mencari user dengan email ini
+            [
+                'name' => 'Admin Absensi',
+                'password' => Hash::make('password')
+            ]
+        );
     }
 }
